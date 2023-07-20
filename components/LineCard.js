b85d017b26183185523b7dc57a7e90d780e949d4
Ismet <ismetmiftari2015@gmail.com>
2023-07-21 00:48:24 +0200

    finished project

--- components/LineCard.js
+++ components/LineCard.js
@@ -3,7 +3,7 @@ import React from "react";
 const LineCard = () => {
   return (
     <div>
-      <div className="flex border-l-8 border-[#E74040] py-4">
+      <div className="flex md:items-center items-center border-l-8 border-[#E74040] py-4">
         <div className="ml-4">
           <div className="w-20 h-20 bg-[#ECECEC] relative rounded-full">
             <p className="absolute top-6 left-7 text-[#E74040] font-bold text-2xl">
