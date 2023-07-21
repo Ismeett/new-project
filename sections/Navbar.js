2b64cf9ca10e2d23b179696e5233e7852dde7aa7
Ismet <ismetmiftari2015@gmail.com>
2023-07-21 03:14:11 +0200

    added navbar in phone mode

--- sections/Navbar.js
+++ sections/Navbar.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   return (
     <div className="container mx-auto pt-3">
-      <div className="flex items-center md:justify-between justify-center">
+      <div className="flex items-center md:justify-between justify-between md:px-0 px-5">
         <Image src="/../public/logo.png" width="125" height="25" />
 
         <ul className="cursor-pointer md:flex space-x-4 w-7/12 text-[#737373] font-bold hidden">
@@ -30,6 +37,33 @@ const Navbar = () => {
             <Image src="/../public/arrow.png" width="14" height="20" />
           </button>
         </div>
+        <Image
+          src="/../public/menu.png"
+          width="40"
+          height="50"
+          className="md:hidden"
+          onClick={handleNav}
+        />
+      </div>
+      <div
+        className={`md:hidden ${
+          nav ? "block" : "hidden"
+        } my-4 flex justify-center items-center text-[#737373] text-lg font-medium text-center`}
+      >
+        <ul>
+          <Link href="/">
+            <li className="my-4 ">Home</li>
+          </Link>
+          <Link href="/">
+            <li className="my-4">Product</li>
+          </Link>
+          <Link href="/">
+            <li className="my-4">Pricing</li>
+          </Link>
+          <Link href="/">
+            <li className="my-4">Contact</li>
+          </Link>
+        </ul>
       </div>
     </div>
   );
